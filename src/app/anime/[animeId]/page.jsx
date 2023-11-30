1653abf33462ca81eb3e34a21d79403f9372d139
rastuak <rastuakfaisal@gmail.com>
2023-12-01 01:00:37 +0700

    added some updates

--- src/app/anime/[animeId]/page.jsx
+++ src/app/anime/[animeId]/page.jsx
@@ -7,13 +7,14 @@ const Page = async ({ params: { animeId } }) => {
     const Animes = await getAnimeResponse(`anime/${animeId}`)
 
     const episodeanime = await getAnimeEpisodeById(animeId)
-    console.log(episodeanime.data)
+    // console.log(episodeanime.data)
+    // console.log(Animes.data.background)
 
 
     return (
         <div className='flex flex-col p-4 gap-3'>
             <br />
-            <h1 className='text-3xl text-center'>{Animes.data.title} - {Animes.data.year}</h1>
+            <h1 className='text-3xl text-center'>{Animes.data.title} - {Animes.data.season} {Animes.data.year}</h1>
             <div className='flex flex-wrap sm:flex-nowrap gap-4 justify-center'>
                 <div className='rounded-md bg-paletAbu-100 p-0.5 shadow-2xl transition-all hover:bg-paletAbu-200'>
                     <Image
@@ -30,14 +31,21 @@ const Page = async ({ params: { animeId } }) => {
                 </div>
             </div>
             <div className='flex justify-center items-center gap-4 overflow-x-auto'>
+                <BoxDesc title='' desc={`${Animes.data.rating}`} />
                 <BoxDesc title='Rank' desc={`${Animes.data.rank}`} />
                 <BoxDesc title='Rating' desc={`${Animes.data.score}`} />
+                <BoxDesc title='Status' desc={`${Animes.data.status}`} />
                 <BoxDesc title='Member' desc={`${Animes.data.members}`} />
                 <BoxDesc title='Episode' desc={`${Animes.data.episodes}`} />
+                <BoxDesc title='Duration' desc={`${Animes.data.duration}`} />
             </div>
             <div className='text-justify self-center rounded-md w-4/5 bg-paletAbu-100 p-2 shadow-2xl transition-all hover:bg-paletAbu-200'>
                 <h1 className='font-bold'> Synopsis: </h1>
                 {Animes.data.synopsis}
+            </div>
+            <div className='text-justify self-center rounded-md w-4/5 bg-paletAbu-100 p-2 shadow-2xl transition-all hover:bg-paletAbu-200'>
+                <h1 className='font-bold'> Episodes: </h1>
+                belom ya ges ya
             </div>
             {/* <div className='self-center p-1 shadow-xl '>
                 <p className='text-xl font-bold'> Trailer: </p>
--- src/components/BoxDesc/BoxDesc.jsx
+++ src/components/BoxDesc/BoxDesc.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BoxDesc = ({title, desc}) => {
   return (
     <>
-    <div className='bg-paletAbu-800 hover:bg-paletAbu-700 h-18 w-24 justify-center items-center text-center p-1.5 rounded-lg shadow-2xl text-white'>
+    <div className='bg-paletAbu-800 hover:bg-paletAbu-700 h-18 w-auto px-2 justify-center items-center text-center py-1 rounded-lg shadow-2xl text-white'>
        <h1> {title} </h1>
        <p> {desc} </p> 
     </div>
